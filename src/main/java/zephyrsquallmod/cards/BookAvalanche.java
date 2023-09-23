01669d280a4fe40e7aa3749ddfcd61ac993ce472
ZephyrSquall <cameron.t.smith.42@gmail.com>
2023-09-23 12:19:35 +0800

    Add card "Book Avalanche"

---
+++ src/main/java/zephyrsquallmod/cards/BookAvalanche.java
@@ -0,0 +1,51 @@
+package zephyrsquallmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import zephyrsquallmod.character.ZephyrSquallCharacter;
+import zephyrsquallmod.util.CardStats;
+
+public class BookAvalanche extends BaseCard {
+    public static final String ID = makeID(BookAvalanche.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            ZephyrSquallCharacter.Enums.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.RARE,
+            CardTarget.ENEMY,
+            3
+    );
+
+    private static final int DAMAGE = 10;
+    private static final int UPG_DAMAGE = 10;
+
+    public BookAvalanche() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+    }
+
+    public void applyPowers() {
+        int realBaseDamage = this.baseDamage;
+        this.baseDamage+= AbstractDungeon.player.masterDeck.size();
+        super.applyPowers();
+        this.baseDamage = realBaseDamage;
+        this.isDamageModified = (this.damage != this.baseDamage);
+    }
+
+    public void calculateCardDamage(AbstractMonster mo) {
+        int realBaseDamage = this.baseDamage;
+        this.baseDamage+= AbstractDungeon.player.masterDeck.size();
+        super.calculateCardDamage(mo);
+        this.baseDamage = realBaseDamage;
+        this.isDamageModified = (this.damage != this.baseDamage);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+    }
+}
