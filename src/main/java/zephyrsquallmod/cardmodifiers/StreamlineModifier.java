1004260197b989fcdde456ecb97408145666756b
ZephyrSquall <cameron.t.smith.42@gmail.com>
2023-09-24 20:53:21 +0800

    Refactor "StreamlineModifier" so it properly handles repeat applications on its own

--- src/main/java/zephyrsquallmod/cardmodifiers/StreamlineModifier.java
+++ src/main/java/zephyrsquallmod/cardmodifiers/StreamlineModifier.java
@@ -3,6 +3,10 @@ package zephyrsquallmod.cardmodifiers;
 import basemod.abstracts.AbstractCardModifier;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
+import java.util.ArrayList;
+
+import static basemod.helpers.CardModifierManager.getModifiers;
+import static basemod.helpers.CardModifierManager.removeSpecificModifier;
 import static zephyrsquallmod.ZephyrSquallMod.makeID;
 
 public class StreamlineModifier extends AbstractCardModifier {
@@ -19,6 +23,36 @@ public class StreamlineModifier extends AbstractCardModifier {
 
     @Override
     public void onInitialApplication(AbstractCard card) {
+        // Check if the card already costs 0 or less (i.e. -1 which means X or -2 which means no cost). Cards that cost
+        // 0 or less cannot be Streamlined further (they still count as Streamlined if they were previously Streamlined,
+        // but won't start counting as Streamlined if they weren't already Streamlined). In this case, simply remove
+        // this modifier immediately with no effect.
+        if (isCardFree(card)) {
+            removeSpecificModifier(card, this, false);
+            return;
+        }
+
+        // Check if the card is already Streamlined. In this case, find the original StreamlineModifier attached to this
+        // card, call its applyAgain method, then remove this modifier with no further effects. It's important to make
+        // sure a card is only ever affected by one Streamline modifier at a time so that when the card is played,
+        // the card's original cost can be restored properly (this is particularly important to make sure the cost of a
+        // card stops displaying in green, but only if no other effects have also changed the cost).
+        ArrayList<AbstractCardModifier> streamlineModifiers = getModifiers(card, makeID(StreamlineModifier.class.getSimpleName()));
+        if (streamlineModifiers.size() >= 2) {
+            // If this ArrayList has 2 elements, that means this modifier and an already-existing modifier were found,
+            // so the card was already Streamlined. Get the StreamlineModifier that isn't this one and call applyAgain
+            // on it.
+            for (AbstractCardModifier streamlineModifier : streamlineModifiers) {
+                if (streamlineModifier != this) {
+                    ((StreamlineModifier) streamlineModifier).applyAgain(card);
+                }
+            }
+            removeSpecificModifier(card, this, false);
+            return;
+        }
+
+        // If this part of the code is reached, this card costs at least 1 energy and hasn't been Streamlined
+        // previously, so set up the Streamline modifier properly.
         if (!card.isCostModified) {
             originallyModified = false;
             originalCost = card.cost;
@@ -26,13 +60,13 @@ public class StreamlineModifier extends AbstractCardModifier {
         reduceCost(card);
     }
 
-    // Do not put multiple applications of StreamlineModifier on a single card, as this interferes with the ability to
-    // properly detect if the card had its cost modified by any other sources. This prevents onRemove from setting
-    // card.isCostModified back to false, resulting in the cost number remaining green even when it's returned to its
-    // original value. If a card is Streamlined again while already Streamlined, call applyAgain on the original
-    // StreamlineModifier instead of applying a second StreamlineModifier.
+    private boolean isCardFree(AbstractCard card) {
+        return card.cost <= 0 || card.costForTurn <= 0 || card.freeToPlayOnce;
+    }
+
     public void applyAgain(AbstractCard card) {
-        reduceCost(card);
+        if (!isCardFree(card))
+            reduceCost(card);
     }
 
     private void reduceCost(AbstractCard card) {
