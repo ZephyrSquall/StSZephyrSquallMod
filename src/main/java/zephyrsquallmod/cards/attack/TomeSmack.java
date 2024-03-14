04340df7fd14affe0285af68f690b657d978dc05
ZephyrSquall <145758512+ZephyrSquall@users.noreply.github.com>
2024-03-14 20:16:56 +0800

    Add card "Tome Smack"

---
+++ src/main/java/zephyrsquallmod/cards/attack/TomeSmack.java
@@ -0,0 +1,41 @@
+package zephyrsquallmod.cards.attack;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import zephyrsquallmod.actions.common.RecordAction;
+import zephyrsquallmod.cards.BaseCard;
+import zephyrsquallmod.cards.skill.Book;
+import zephyrsquallmod.character.ZephyrSquallCharacter;
+import zephyrsquallmod.util.CardStats;
+
+public class TomeSmack extends BaseCard {
+    public static final String ID = makeID(TomeSmack.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            ZephyrSquallCharacter.Enums.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.COMMON,
+            CardTarget.ENEMY,
+            1
+    );
+
+    private static final int DAMAGE = 7;
+    private static final int UPG_DAMAGE = 10;
+    private static final int MAGIC = 1;
+
+    public TomeSmack() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+        setMagic(MAGIC);
+        this.cardsToPreview = new Book();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new RecordAction(this.magicNumber, false, false));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+    }
+}
