d193a8dd79408c7d8d0a5602e5d11c9a0a8c89d5
ZephyrSquall <145758512+ZephyrSquall@users.noreply.github.com>
2025-01-28 20:23:59 +0800

    Add card "Feint"

---
+++ src/main/java/zephyrsquallmod/cards/skill/Feint.java
@@ -0,0 +1,36 @@
+package zephyrsquallmod.cards.skill;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveAllBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import zephyrsquallmod.cards.BaseCard;
+import zephyrsquallmod.character.ZephyrSquallCharacter;
+import zephyrsquallmod.util.CardStats;
+
+public class Feint extends BaseCard {
+    public static final String ID = makeID(Feint.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            ZephyrSquallCharacter.Enums.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.ENEMY,
+            1
+    );
+
+    private static final int MAGIC = 1;
+    private static final int UPG_MAGIC = 1;
+
+    public Feint() {
+        super(ID, info);
+
+        setMagic(MAGIC, UPG_MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new RemoveAllBlockAction(m, p));
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
+    }
+}
