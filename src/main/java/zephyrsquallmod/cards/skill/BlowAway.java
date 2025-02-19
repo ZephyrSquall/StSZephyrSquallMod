fb250ef5d408f7afb5736cc61865f690c7a81907
ZephyrSquall <145758512+ZephyrSquall@users.noreply.github.com>
2025-02-19 15:15:08 +0800

    Rework Headwind power

--- src/main/java/zephyrsquallmod/cards/skill/BlowAway.java
+++ src/main/java/zephyrsquallmod/cards/skill/BlowAway.java
@@ -6,7 +6,6 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import zephyrsquallmod.cards.BaseCard;
 import zephyrsquallmod.character.ZephyrSquallCharacter;
 import zephyrsquallmod.powers.HeadwindPower;
-import zephyrsquallmod.powers.TailwindPower;
 import zephyrsquallmod.util.CardStats;
 
 public class BlowAway extends BaseCard {
@@ -19,25 +18,18 @@ public class BlowAway extends BaseCard {
             2
     );
 
-    private static final int MAGIC = 1;
+    private static final int MAGIC = 10;
+    private static final int UPG_MAGIC = 3;
 
     public BlowAway() {
         super(ID, info);
 
-        setMagic(MAGIC);
+        setMagic(MAGIC, UPG_MAGIC);
         setExhaust(true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(m, p, new HeadwindPower(p)));
-        if (upgraded)
-            addToBot(new ApplyPowerAction(p, p, new TailwindPower(p, magicNumber)));
-    }
-
-    @Override
-    public void upgrade() {
-        super.upgrade();
-        this.target = CardTarget.SELF_AND_ENEMY;
+        addToBot(new ApplyPowerAction(m, p, new HeadwindPower(m, magicNumber)));
     }
 }
--- src/main/java/zephyrsquallmod/powers/HeadwindPower.java
+++ src/main/java/zephyrsquallmod/powers/HeadwindPower.java
@@ -1,30 +1,39 @@
 package zephyrsquallmod.powers;
 
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import static zephyrsquallmod.ZephyrSquallMod.makeID;
 
 public class HeadwindPower extends BasePower {
     public static final String POWER_ID = makeID("Headwind");
     private static final PowerType TYPE = PowerType.DEBUFF;
-    private static final boolean TURN_BASED = false;
+    private static final boolean TURN_BASED = true;
 
-    public HeadwindPower(AbstractCreature owner) {
-        super(POWER_ID, TYPE, TURN_BASED, owner, 1);
+    public HeadwindPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, amount);
     }
 
     public void updateDescription() {
-        if (amount == 1)
-            this.description = DESCRIPTIONS[0];
-        else
-            this.description = DESCRIPTIONS[1] + amount + DESCRIPTIONS[2];
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    public void atEndOfRound() {
+        int targetAmount = this.amount / 2;
+        if (targetAmount == 0) {
+            addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, HeadwindPower.POWER_ID));
+        } else {
+            // reductionAmount may differ from targetAmount due to rounding, so it has to be calculated separately.
+            int reductionAmount = this.amount - targetAmount;
+            addToBot(new ReducePowerAction(this.owner, this.owner, HeadwindPower.POWER_ID, reductionAmount));
+        }
     }
 
     public float atDamageGive(float damage, DamageInfo.DamageType type) {
-        if (type == DamageInfo.DamageType.NORMAL && AbstractDungeon.player.hasPower(TailwindPower.POWER_ID))
-            return damage - (this.amount * AbstractDungeon.player.getPower(TailwindPower.POWER_ID).amount);
+        if (type == DamageInfo.DamageType.NORMAL)
+            return damage - this.amount;
         return damage;
     }
 }
