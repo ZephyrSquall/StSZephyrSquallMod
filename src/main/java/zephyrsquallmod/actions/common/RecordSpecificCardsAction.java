f8d4fc0fc3af8304b8bac70a37416c41d5bb1620
ZephyrSquall <145758512+ZephyrSquall@users.noreply.github.com>
2025-02-21 20:25:32 +0800

    Fix Record action creating an empty Book if 0 cards are chosen

--- src/main/java/zephyrsquallmod/actions/common/RecordSpecificCardsAction.java
+++ src/main/java/zephyrsquallmod/actions/common/RecordSpecificCardsAction.java
@@ -20,28 +20,32 @@ public class RecordSpecificCardsAction extends AbstractGameAction {
     }
 
     public void update() {
-        // Make sure Books aren't recorded again by removing any books from the group.
-        cards.removeIf(card -> card.cardID.equals(Book.ID));
-        addToTop(new MakeTempCardInHandAction(new Book(cards), false, true));
-        for (AbstractCard card : cards) {
-            // What follows is a direct copy-paste of the resetCardBeforeMoving method on the cardGroup class. Ideally
-            // I'd call this method directly, but I can't because it's private. Normally calling the method directly
-            // isn't needed because public methods are provided for moving cards from hand into the draw, discard, or
-            // exhaust pile, and these public methods indirectly call the resetCardBeforeMoving method. However, there
-            // is no such public method for removing cards from combat entirely, as this never happens in the base game
-            // (except for Bronze Orbs putting cards in stasis, which never removes cards from the player's hand).
-            if (AbstractDungeon.player.hoveredCard == card)
-                AbstractDungeon.player.releaseCard();
-            AbstractDungeon.actionManager.removeFromQueue(card);
-            card.unhover();
-            card.untip();
-            card.stopGlowing();
-            this.cardGroup.group.remove(card);
-            // End of resetCardBeforeMoving method copy-paste.
+        // Don't create a Book if 0 cards were provided to be Recorded.
+        if (!cards.isEmpty()) {
+            // Make sure Books aren't recorded again by removing any books from the group.
+            cards.removeIf(card -> card.cardID.equals(Book.ID));
+            addToTop(new MakeTempCardInHandAction(new Book(cards), false, true));
+            for (AbstractCard card : cards) {
+                // What follows is a direct copy-paste of the resetCardBeforeMoving method on the cardGroup class.
+                // Ideally I'd call this method directly, but I can't because it's private. Normally calling the method
+                // directly isn't needed because public methods are provided for moving cards from hand into the draw,
+                // discard, or exhaust pile, and these public methods indirectly call the resetCardBeforeMoving method.
+                // However, there is no such public method for removing cards from combat entirely, as this never
+                // happens in the base game (except for Bronze Orbs putting cards in stasis, which never removes cards
+                // from the player's hand).
+                if (AbstractDungeon.player.hoveredCard == card)
+                    AbstractDungeon.player.releaseCard();
+                AbstractDungeon.actionManager.removeFromQueue(card);
+                card.unhover();
+                card.untip();
+                card.stopGlowing();
+                this.cardGroup.group.remove(card);
+                // End of resetCardBeforeMoving method copy-paste.
 
-            if (card.type != AbstractCard.CardType.CURSE && card.type != AbstractCard.CardType.STATUS && AbstractDungeon.player.hasRelic(ResearchPaper.ID)) {
-                AbstractDungeon.player.getRelic(ResearchPaper.ID).flash();
-                card.upgrade();
+                if (card.type != AbstractCard.CardType.CURSE && card.type != AbstractCard.CardType.STATUS && AbstractDungeon.player.hasRelic(ResearchPaper.ID)) {
+                    AbstractDungeon.player.getRelic(ResearchPaper.ID).flash();
+                    card.upgrade();
+                }
             }
         }
         this.isDone = true;
